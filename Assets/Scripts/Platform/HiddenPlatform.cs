e5e9d1b7a21a5993b1e2bfb2a144df7bba6e2498
Huyhsu <siu891014@gmail.com>
2022-01-28 19:26:55 +0800

    Add dotween asset

--- Assets/Scripts/Platform/HiddenPlatform.cs
+++ Assets/Scripts/Platform/HiddenPlatform.cs
@@ -1,18 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class HiddenPlatform : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Tilemap _tilemap;
+    private float _animationTime = 0.5f;
+    private Tween _tween;
+
+    private void Awake()
     {
-        
+        _tilemap = GetComponent<Tilemap>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
+        if (!other.CompareTag("Player")) return;
         
+        _tween?.Kill();
+        _tween = DOTween.To(() => _tilemap.color, (value) => _tilemap.color = value, new Color(1, 1, 1, 0), _animationTime)
+            .SetEase(Ease.Linear)
+            .SetUpdate(true);
+    }
+    
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player")) return;
+        
+        _tween?.Kill();
+        _tween = DOTween.To(() => _tilemap.color, (value) => _tilemap.color = value, new Color(1, 1, 1, 1), _animationTime)
+            .SetEase(Ease.Linear)
+            .SetUpdate(true);
+    }
+
+    private void OnDestroy()
+    {
+        _tween?.Kill();
     }
 }
